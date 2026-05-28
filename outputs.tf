4c1b83fd51e31f989428ea06bb7650061d315057
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 15:36:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,11 +5,3 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
-output "public_subnets" {
-  value = module.vpc.public_subnets
-}
