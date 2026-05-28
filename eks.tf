43d5702052a5fad59e9cf96aa26167c13f9448e6
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 23:06:16 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -7,6 +7,7 @@ module "eks" {
 
   cluster_encryption_config = []
   create_cloudwatch_log_group = false
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
