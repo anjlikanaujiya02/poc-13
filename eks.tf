f6d2f36a7e480243e9c206df398bff6fba5e190e
ayuhcl <ayushi-agarwal@hcltech.com>
2026-05-28 09:40:31 +0530

    Create eks.tf

---
+++ eks.tf
@@ -0,0 +1,19 @@
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+
+  subnet_ids = module.vpc.public_subnets
+  vpc_id     = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 1
+      max_size     = 2
+      min_size     = 1
+
+      instance_types = ["t3.micro"]
+    }
+  }
+}
